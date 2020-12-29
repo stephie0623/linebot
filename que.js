f01fd1bf116a2e4dc45a35e56a8cc9be139357dd
stephie0623 <49089659+stephie0623@users.noreply.github.com>
2020-12-29 19:39:04 +0800

    1

---
+++ que.js
@@ -0,0 +1,20 @@
+function Appear() {
+    //获取随机数
+    var luckyNum = Math.round(Math.random() * 7);
+    //alert(luckyNum);
+    if (luckyNum == 0) {
+        window.location.href = "./ans/ans1.html";
+    } else if (luckyNum == 1) {
+        window.location.href = "./ans/ans2.html";
+    } else if (luckyNum == 2) {
+        window.location.href = "./ans/ans3.html";
+    } else if (luckyNum == 3) {
+        window.location.href = "./ans/ans4.html";
+    } else if (luckyNum == 4) {
+        window.location.href = "./ans/ans.html";
+    } else if (luckyNum == 5) {
+        window.location.href = "./ans/ans6.html";
+    } else if (luckyNum == 6) {
+        window.location.href = "./ans/ans7.html";
+    }
+}
